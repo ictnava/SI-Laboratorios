e87f9f6f38c6a34000b5216a0c5e08a35688d43a
Antonio Varela <sanguinetiara31@gmail.com>
2019-05-05 15:30:00 -0500

    Add files via upload

--- consultaAnuncios.php
+++ consultaAnuncios.php
@@ -20,5 +20,34 @@
         <?php echo $_GET['msg']; ?>
     </div>
     <?php endif; ?>
+   
+    <div id="divConsEntrad">
+            <h2>consuta de anuncios</h2>
+            <div class="card-group">
+            <?php 
+              $bd = new BaseDatos();
+              $bd->conectar();
+              if(!$bd->getConexion())
+                echo "Error al procesar la petición :( ";
+              $qry = "SELECT Nombre, Descripcion, Imagen FROM Anuncios 
+                        INNER JOIN laboratorio ON registro_alumno.ClaveLab = laboratorio.ClaveLab";
+              $sentencia = $bd->getConexion()->prepare($qry);
+              $rs = $sentencia->execute();
+              if(!$rs)
+                print_r($sentencia->errorInfo());
+              while($datos= $sentencia->fetchObject())
+              {
+                  echo "<div class='card'>";
+                  echo "<img class='card-img-top' src='.$datos->Imagen' alt='Card image cap'>";
+                  echo "<div class='card-body'>";
+                  echo "<h5 class='card-title'>.$datos->Nombre</h5>";
+                  echo "<p class='card-text'>.$datos->Describcion</p>";
+                  echo "</div>";
+                  echo "</div>";
+              }
+              $bd->desconectar();
+            ?>
+            </div>
+            </div>
 </body>
 </html>
